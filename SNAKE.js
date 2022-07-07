252a9e0d33ddc9b234ecce23bded2a9a25ad22a6
Alexander Ab <ab.alexande21@gmail.com>
2022-07-07 16:09:10 -0400

    created functions to draw snake and apple

--- SNAKE.js
+++ SNAKE.js
@@ -9,3 +9,31 @@ for (let i = 0; i < 625; i++) {
   gameBoard.appendChild(div)
 }
 drawBoard()
+drawApple()
+drawSnake()
+
+//Draws a snake in the first 3 boxes
+function drawSnake() {
+  let boxes = document.querySelectorAll('#grid div')
+  console.log(boxes.length)
+  snakePosition = [2, 1, 0]
+  snakePosition.forEach((index) => {
+    boxes[index].classList.add('snake')
+  })
+}
+
+//Draws an apple in a random box
+function drawApple() {
+  let boxes = document.querySelectorAll('#grid div')
+  applePosition = [Math.floor(Math.random() * boxes.length)]
+  applePosition.forEach((index) => {
+    boxes[index].classList.add('apple')
+  })
+}
+
+function moveSnake() {
+  let boxes = document.querySelectorAll('#grid div')
+  const update = snakePosition.pop()
+  boxes[update].classList.remove('snake')
+  snakePosition.unshift(snakePosition[0] + 1)
+}
