8a0f8dfe4b3145fdf4677e1e50128b245f901ea0
Alexander Ab <ab.alexande21@gmail.com>
2022-07-07 22:31:36 -0400

    added event listeners to move around the snake

--- SNAKE.js
+++ SNAKE.js
@@ -2,38 +2,66 @@ const gameBoard = document.getElementById('grid')
 const playAgain = document.getElementById('play-again')
 const scoreCount = document.getElementById('Score')
 
-//Creates a 25x25 board with each div ('box') being 32px
+let interval = 0
+let speedX = 0
+
+// Creates a 25x25 board with each div ('box') being 32px
 function drawBoard() {}
 for (let i = 0; i < 625; i++) {
   let div = document.createElement('div')
   gameBoard.appendChild(div)
 }
+
+const boxes = document.querySelectorAll('#grid div')
+
 drawBoard()
 drawApple()
 drawSnake()
+main()
+
+function main() {
+  intervelTime = 300
+  moveSnake()
+  // checkIfAppleEaten()
+  interval = setInterval(moveSnake, intervelTime)
+}
+
+function moveSnake() {
+  const update = snakePosition.pop()
+  boxes[update].classList.remove('snake')
+  snakePosition.unshift(snakePosition[0] + speedX)
+  snakePosition.forEach((i) => {
+    boxes[i].classList.add('snake')
+  })
+}
 
 //Draws a snake in the first 3 boxes
 function drawSnake() {
-  let boxes = document.querySelectorAll('#grid div')
   console.log(boxes.length)
-  snakePosition = [2, 1, 0]
-  snakePosition.forEach((index) => {
-    boxes[index].classList.add('snake')
+  snakePosition = [2, 1]
+  snakePosition.forEach((i) => {
+    boxes[i].classList.add('snake')
   })
 }
 
 //Draws an apple in a random box
 function drawApple() {
-  let boxes = document.querySelectorAll('#grid div')
   applePosition = [Math.floor(Math.random() * boxes.length)]
-  applePosition.forEach((index) => {
-    boxes[index].classList.add('apple')
+  applePosition.forEach((i) => {
+    boxes[i].classList.add('apple')
   })
 }
 
-function moveSnake() {
-  let boxes = document.querySelectorAll('#grid div')
-  const update = snakePosition.pop()
-  boxes[update].classList.remove('snake')
-  snakePosition.unshift(snakePosition[0] + 1)
+function changeDirection(event) {
+  if (event.key === 'ArrowDown') {
+    speedX = +25
+  } else if (event.key === 'ArrowUp') {
+    speedX = -25
+  } else if (event.key === 'ArrowLeft') {
+    speedX = -1
+  } else if (event.key === 'ArrowRight') {
+    speedX = 1
+  }
 }
+
+document.addEventListener('keydown', changeDirection)
